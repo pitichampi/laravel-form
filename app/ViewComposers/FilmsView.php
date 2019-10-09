a386709a3b1787dd8fd487169104d136a5734971
Pitichampi <contact@pitichampi.fr>
2019-10-09 11:22:58 +0200

    différentes manières de requêter la base.

--- app/ViewComposers/FilmsView.php
+++ app/ViewComposers/FilmsView.php
@@ -23,7 +23,8 @@ use Illuminate\Http\Request;
 use Illuminate\View\View;
 //use App\Repositories\FilmsRepository;
 use App\Models\Films;
-
+use DB;
+use PDO;
 
 use App\Facades\CounterFacade;
 class FilmsView
@@ -37,7 +38,27 @@ class FilmsView
     public function compose(View $view){
 
         //$listefilms=FilmsRepository::$liste;
-        $listefilms = Films::all();  //récupère toutes les données de la table films
+        $listefilms = Films::all();  //récupère toutes les données de la table films - Pas besoin d'utiliser la méthode get pour ce cas.
+        //$listefilms = Films::orderby('annee','desc')->orderby('titre','asc')->get();  // un peu de tri. Attention ! il faut utiliser la méthode get.
+        //$listefilms = Films::where('annee',2010)->orderby('titre','asc')->get();  // where
+        //$listefilms = Films::where('annee',2010)->orderby('titre','asc')->first();  // first au lieu de get. Attention ne retourne pas un tableau d'objets mais l'objet directement.
+        //$listefilms = Films::where('titre','like','%tour%')->orderby('titre','asc')->get();  // first au lieu de get. Attention ne retourne pas un tableau d'objets mais l'objet directement.
+
+        //Pour des requêtes plus complexes : façade DB.
+        //    $listefilms=DB::table('films')->select('*')->orderby('annee')->get();
+        // Par contre la réponse retournée n'est plus un tableau associatif. Attention donc.
+
+        // Une requête standard !
+        //$listefilms=DB::select(DB::raw("SELECT * FROM films WHERE annee >= 2010"));
+        // Ce coup-ci pas besoin de get. Par contre penser au use DB !
+
+        // Une méthode qui ne change pas le type de réponse (retourne bien un tableau associatif :
+        //$filmsData=DB::connection()->getPDO()->query('select * from films');
+        //$listefilms=$filmsData->fetchAll(PDO::FETCH_ASSOC);
+        // Attention rajouter un use PDO en plus du use DB. En fait on utilise simplement l'objet PDO standard.
+
+
+
         $count=CounterFacade::getCounter($this->request);
         $view->with(['films' => $listefilms, 'count' => $count]);
 
