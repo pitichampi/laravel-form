3f3d1206dea595fd3d92a5002522ff26fab41a49
Pitichampi <contact@pitichampi.fr>
2019-10-10 10:32:01 +0200

    mutator et accesseur

--- app/Models/Films.php
+++ app/Models/Films.php
@@ -10,12 +10,7 @@ class Films extends Model
     //
     protected $table="films";   // Facultatif. Si on ne précise pas cette info, il prend le nom du modèle en minuscules. Si le nom du modèle n'a pas de 's' (singulier) il matche avec la table au pluriel.
     protected $fillable=['titre','auteur','genre','img','annee','synopsys', 'duree'];
-
-    public function setTitleAttribute($value){
-        $this->attributes['title']=strtolower($value);
-    }
-
-
+    a
     protected static function boot()
     {
         parent::boot();
@@ -26,6 +21,18 @@ class Films extends Model
     public static function scopeFilms($query){
         return $query->where('annee','<','2001');
     }
+
+    //Accesseur (getter)
+    public function getTitreAttribute($value)
+    {
+        return strtoupper($value);
+    }
+
+    //mutateur
+    public function setTitreAttribute($value){
+        $this->attributes['titre']=strtolower($value);
+    }
+
 
     public function Immatriculations(){
         return $this->hasOne('App\Models\Immatriculations');
