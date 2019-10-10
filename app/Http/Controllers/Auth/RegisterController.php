4685bdcaea562937e9c2bfd77a945ef6230d4aa2
Pitichampi <contact@pitichampi.fr>
2019-10-10 15:35:27 +0200

    authentification par token

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -68,6 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'username' => $data['username'],
+            'api_token' => Str::random(60),
             'password' => Hash::make($data['password']),
         ]);
     }
